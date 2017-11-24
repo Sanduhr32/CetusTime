50115764178e4af3753a6f1c4178915fe73ea046
Michael Ritter <ritter.michael92@gmail.com>
2017-11-24 19:00:49 +0100

    Exported all the listener code to its own class (cleanup)

---
+++ src/main/java/com/kantenkugel/cetustime/Listener.java
@@ -0,0 +1,161 @@
+package com.kantenkugel.cetustime;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.ISnowflake;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.events.ReconnectedEvent;
+import net.dv8tion.jda.core.events.channel.text.TextChannelDeleteEvent;
+import net.dv8tion.jda.core.events.guild.GuildLeaveEvent;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
+import static com.kantenkugel.cetustime.Main.LOG;
+
+public class Listener extends ListenerAdapter {
+
+    private void setup(Event event) {
+        JDA jda = event.getJDA();
+        Main.JDA = jda;
+        LOG.info("Initializing messages");
+        List<String> idsToDelete = new ArrayList<>(Config.getChannelIds().size());
+        for(String channelId : Config.getChannelIds()) {
+            TextChannel tc = jda.getTextChannelById(channelId);
+            if(tc == null) {
+                idsToDelete.add(channelId);
+                continue;
+            }
+
+            AtomicReference<Message> botMsg = new AtomicReference<>(null);
+            AtomicInteger prevMsgs = new AtomicInteger(0);
+            tc.getIterableHistory().cache(false).forEachRemaining(m -> {
+                if(m.getAuthor() == jda.getSelfUser()) {
+                    botMsg.set(m);
+                    return false;
+                }
+                return (prevMsgs.incrementAndGet() < 200);
+            });
+
+            Message msg = botMsg.get();
+
+            if(msg == null || prevMsgs.get() > 0) {
+                LOG.debug("Scanned {} messages", prevMsgs.get());
+                if(msg != null && prevMsgs.get() > 0) {
+                    LOG.info("Deleting outdated msg in {}...", channelId);
+                    msg.delete().queue();
+                    msg = null;
+                } else {
+                    LOG.info("Creating new message for tc {}...", channelId);
+                }
+            }
+            Main.getMap().put(channelId, msg == null ? 0L : msg.getIdLong());
+        }
+        if(idsToDelete.size() > 0) {
+            idsToDelete.forEach(Config::removeChannel);
+            Config.write();
+            LOG.info("Cleaned up {} old TCs", idsToDelete.size());
+        }
+        LOG.info("Done with setup... starting up scheduled task");
+        Main.setupExecutor();
+    }
+
+    @Override
+    public void onReady(ReadyEvent event) {
+        setup(event);
+    }
+
+    @Override
+    public void onReconnect(ReconnectedEvent event) {
+        setup(event);
+    }
+
+    @Override
+    public void onPrivateMessageReceived(PrivateMessageReceivedEvent e) {
+        if(Config.getAdminIds().contains(e.getAuthor().getId()) && e.getMessage().getContent().equals("shutdown")) {
+            LOG.info("Admin {} ({}) issued shutdown... ", e.getAuthor().getName(), e.getAuthor().getIdLong());
+            e.getJDA().shutdown();
+        }
+    }
+
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
+        if(e.getAuthor().isBot() || e.getAuthor().isFake())
+            return;
+
+        Map<String, Long> map = Main.getMap();
+
+        //handle commands
+        if(e.getMessage().getRawContent().equals(e.getJDA().getSelfUser().getAsMention() + " track")) {
+            String chanId = e.getChannel().getId();
+            if(map.containsKey(chanId)) {
+                map.remove(chanId);
+                Config.removeChannel(chanId);
+                Config.write();
+                e.getChannel().sendMessage("No longer tracking here...").queue();
+                LOG.info("No longer tracking channel {} ({})", e.getChannel(), chanId);
+            } else {
+                map.put(chanId, 0L);
+                Config.addChannel(chanId);
+                Config.write();
+                LOG.info("Now tracking channel {} ({})", e.getChannel(), chanId);
+            }
+        }
+        //TODO: handle other commands
+
+        //handle keepOnBottom
+        TextChannel channel = e.getChannel();
+        if(Config.isKeepOnBottom() && Config.getChannelIds().contains(channel.getId())) {
+            if(map.get(channel.getId()) == 0L)
+                return;
+            LOG.debug("Renewing message in tc {}", channel.getIdLong());
+            map.compute(channel.getId(), (key, value) -> {
+                if(value != 0L)
+                    channel.deleteMessageById(value).queueAfter(1, TimeUnit.SECONDS);
+                return 0L;
+            });
+        }
+    }
+
+    @Override
+    public void onGuildLeave(GuildLeaveEvent e) {
+        Map<String, Long> map = Main.getMap();
+        Set<String> currIds = map.keySet();
+        List<String> idsToRemove = e.getGuild().getTextChannels().stream()
+                .map(ISnowflake::getId)
+                .filter(currIds::contains)
+                .collect(Collectors.toList());
+        idsToRemove.forEach(id -> {
+            map.remove(id);
+            Config.removeChannel(id);
+        });
+        if(idsToRemove.size() > 0) {
+            Config.write();
+            LOG.info("Left guild {} and removed {} tracking channels", e.getGuild().getName(), idsToRemove.size());
+        }
+    }
+
+    @Override
+    public void onTextChannelDelete(TextChannelDeleteEvent event) {
+        TextChannel tc = event.getChannel();
+        String id = tc.getId();
+        Map<String, Long> map = Main.getMap();
+        if(map.containsKey(id)) {
+            map.remove(id);
+            Config.removeChannel(id);
+            Config.write();
+            LOG.info("Tracking channel {} ({}) was deleted and therefore is no longer tracked", tc.getName(), id);
+        }
+    }
+}
--- src/main/java/com/kantenkugel/cetustime/Main.java
+++ src/main/java/com/kantenkugel/cetustime/Main.java
@@ -3,18 +3,8 @@ package com.kantenkugel.cetustime;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.entities.ISnowflake;
-import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageEmbed;
-import net.dv8tion.jda.core.entities.TextChannel;
-import net.dv8tion.jda.core.events.Event;
-import net.dv8tion.jda.core.events.ReadyEvent;
-import net.dv8tion.jda.core.events.channel.text.TextChannelDeleteEvent;
-import net.dv8tion.jda.core.events.guild.GuildLeaveEvent;
-import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
-import net.dv8tion.jda.core.hooks.EventListener;
 import net.dv8tion.jda.core.utils.JDALogger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,11 +15,8 @@ import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
 
-public class Main implements EventListener {
+public class Main {
     public static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public static final ScheduledExecutorService EXECUTOR = Executors.newScheduledThreadPool(1, r -> {
@@ -38,16 +25,28 @@ public class Main implements EventListener {
         return t;
     });
 
-    private static JDA jda;
+    static JDA JDA;
     private static Map<String, Long> channelMsgMap = new HashMap<>();
 
     //Starting up with 30mib of memory should be sufficient (-Xmx30M)
     public static void main(String[] args) {
         try {
-            new JDABuilder(AccountType.BOT).setToken(Config.getBotToken()).addEventListener(new Main()).buildAsync();
+            new JDABuilder(AccountType.BOT).setToken(Config.getBotToken()).addEventListener(new Listener()).buildAsync();
         } catch(LoginException | RateLimitedException e) {
             LOG.error("Error starting up bot", e);
         }
+    }
+
+    static Map<String, Long> getMap() {
+        return channelMsgMap;
+    }
+
+    private static boolean setup = false;
+    static synchronized void setupExecutor() {
+        if(setup)
+            return;
+        EXECUTOR.scheduleAtFixedRate(Main::update, 0, 2, TimeUnit.SECONDS);
+        setup = true;
     }
 
     private static void update() {
@@ -70,9 +69,9 @@ public class Main implements EventListener {
             return;
         channelMsgMap.forEach((key, value) -> {
             if(value == 0L) {
-                channelMsgMap.put(key, jda.getTextChannelById(key).sendMessage(embed).complete().getIdLong());
+                channelMsgMap.put(key, JDA.getTextChannelById(key).sendMessage(embed).complete().getIdLong());
             } else {
-                jda.getTextChannelById(key).editMessageById(value, embed).queue();
+                JDA.getTextChannelById(key).editMessageById(value, embed).queue();
             }
         });
         LOG.trace("Current memory stats (in kib): Total: {}, Free: {}, Usage: {}",
@@ -80,115 +79,5 @@ public class Main implements EventListener {
                 JDALogger.getLazyString(() -> Long.toString(Runtime.getRuntime().freeMemory()/1024)),
                 JDALogger.getLazyString(() -> Long.toString((Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory())/1024))
         );
-    }
-
-    @Override
-    public void onEvent(Event event) {
-        if(event instanceof ReadyEvent) {
-            jda = event.getJDA();
-            LOG.info("Initializing messages");
-            List<String> idsToDelete = new ArrayList<>(Config.getChannelIds().size());
-            for(String channelId : Config.getChannelIds()) {
-                TextChannel tc = jda.getTextChannelById(channelId);
-                if(tc == null) {
-                    idsToDelete.add(channelId);
-                    continue;
-                }
-
-                AtomicReference<Message> botMsg = new AtomicReference<>(null);
-                AtomicInteger prevMsgs = new AtomicInteger(0);
-                tc.getIterableHistory().cache(false).forEachRemaining(m -> {
-                    if(m.getAuthor() == jda.getSelfUser()) {
-                        botMsg.set(m);
-                        return false;
-                    }
-                    return (prevMsgs.incrementAndGet() < 200);
-                });
-
-                Message msg = botMsg.get();
-
-                if(msg == null || prevMsgs.get() > 0) {
-                    LOG.debug("Scanned {} messages", prevMsgs.get());
-                    if(msg != null && prevMsgs.get() > 0) {
-                        LOG.info("Deleting outdated msg in {}...", channelId);
-                        msg.delete().queue();
-                        msg = null;
-                    } else {
-                        LOG.info("Creating new message for tc {}...", channelId);
-                    }
-                }
-                channelMsgMap.put(channelId, msg == null ? 0L : msg.getIdLong());
-            }
-            if(idsToDelete.size() > 0) {
-                idsToDelete.forEach(Config::removeChannel);
-                Config.write();
-                LOG.info("Cleaned up {} old TCs", idsToDelete.size());
-            }
-            LOG.info("Done with setup... starting up scheduled task");
-            EXECUTOR.scheduleAtFixedRate(Main::update, 0, 2, TimeUnit.SECONDS);
-        } else if(event instanceof PrivateMessageReceivedEvent) {
-            PrivateMessageReceivedEvent e = (PrivateMessageReceivedEvent) event;
-            if(Config.getAdminIds().contains(e.getAuthor().getId()) && e.getMessage().getContent().equals("shutdown")) {
-                LOG.info("Admin {} ({}) issued shutdown... ", e.getAuthor().getName(), e.getAuthor().getIdLong());
-                jda.shutdown();
-            }
-        } else if(event instanceof GuildMessageReceivedEvent) {
-            GuildMessageReceivedEvent e = (GuildMessageReceivedEvent) event;
-            if(e.getAuthor().isBot() || e.getAuthor().isFake())
-                return;
-            if(Config.getAdminIds().contains(e.getAuthor().getId())) {
-                if(e.getMessage().getRawContent().equals(e.getJDA().getSelfUser().getAsMention() + " track")) {
-                    String chanId = e.getChannel().getId();
-                    if(channelMsgMap.containsKey(chanId)) {
-                        channelMsgMap.remove(chanId);
-                        Config.removeChannel(chanId);
-                        Config.write();
-                        e.getChannel().sendMessage("No longer tracking here...").queue();
-                        LOG.info("No longer tracking channel {} ({})", e.getChannel(), chanId);
-                    } else {
-                        channelMsgMap.put(chanId, 0L);
-                        Config.addChannel(chanId);
-                        Config.write();
-                        LOG.info("Now tracking channel {} ({})", e.getChannel(), chanId);
-                    }
-                }
-                //TODO: handle special commands
-            }
-            TextChannel channel = e.getChannel();
-            if(Config.isKeepOnBottom() && Config.getChannelIds().contains(channel.getId())) {
-                if(channelMsgMap.get(channel.getId()) == 0L)
-                    return;
-                LOG.debug("Renewing message in tc {}", channel.getIdLong());
-                channelMsgMap.compute(channel.getId(), (key, value) -> {
-                    if(value != 0L)
-                        channel.deleteMessageById(value).queueAfter(1, TimeUnit.SECONDS);
-                    return 0L;
-                });
-            }
-        } else if(event instanceof GuildLeaveEvent) {
-            GuildLeaveEvent e = (GuildLeaveEvent) event;
-            Set<String> currIds = channelMsgMap.keySet();
-            List<String> idsToRemove = e.getGuild().getTextChannels().stream()
-                    .map(ISnowflake::getId)
-                    .filter(currIds::contains)
-                    .collect(Collectors.toList());
-            idsToRemove.forEach(id -> {
-                channelMsgMap.remove(id);
-                Config.removeChannel(id);
-            });
-            if(idsToRemove.size() > 0) {
-                Config.write();
-                LOG.info("Left guild {} and removed {} tracking channels", e.getGuild().getName(), idsToRemove.size());
-            }
-        } else if(event instanceof TextChannelDeleteEvent) {
-            TextChannel tc = ((TextChannelDeleteEvent) event).getChannel();
-            String id = tc.getId();
-            if(channelMsgMap.containsKey(id)) {
-                channelMsgMap.remove(id);
-                Config.removeChannel(id);
-                Config.write();
-                LOG.info("Tracking channel {} ({}) was deleted and therefore is no longer tracked", tc.getName(), id);
-            }
-        }
     }
 }
